fb1f663334d83e9c84eae1d36d7cc58d775890a0
dolly17107 <dolly17107@gmail.com>
2021-02-15 08:26:30 +0000

    Fix typo

--- dolly17107.select.js
+++ dolly17107.select.js
@@ -1,15 +1,15 @@
 export class Dolly17107Select extends HTMLElement {
     select(node) {
-        this.querySelectorAll(":scope > [data-select]").forEach(node => { node.removeAttribute("data-selected"); });
+        this.querySelectorAll(":scope > [data-selected]").forEach(node => { node.removeAttribute("data-selected"); });
         node.dataset.selected = "";
         this.dispatchEvent(new CustomEvent("change")); }
     constructor() {
         super();
         this.tabIndex = 0;
-        this.addEventListener("click", function(e) { this.focus() }); } }
+        this.addEventListener("click", function(e) { this.focus(); this.dataset.expanded = ""; }); } }
 export class Dolly17107Option extends HTMLElement {
     constructor() {
         super();
-        this.addEventListener("click", function(e) { this.parentNode.select(this); }); } }
+        this.addEventListener("click", function(e) { this.parentNode.select(this); this.parentNode.removeAttribute("data-expanded"); }); } }
 customElements.define("dolly17107-select", Dolly17107Select);
 customElements.define("dolly17107-option", Dolly17107Option);
