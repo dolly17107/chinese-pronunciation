57b0653ccd095b9407e378d8011a8fafc3d34eb1
dolly17107 <dolly17107@gmail.com>
2021-02-15 07:59:36 +0000

    Add a select box for different periods for which to transcribe

---
+++ dolly17107.select.js
@@ -0,0 +1,14 @@
+export class Dolly17107Select extends HTMLElement {
+    select(node) {
+        this.querySelectorAll(":scope > [data-select]").forEach(node => { node.removeAttribute("data-selected"); });
+        node.dataset.selected = "";
+        this.dispatchEvent(new CustomEvent("change")); }
+    constructor() {
+        super();
+        this.tabIndex = 0; } }
+export class Dolly17107Option extends HTMLElement {
+    constructor() {
+        super();
+        this.addEventListener("click", function(e) { this.parentNode.select(this); }); } }
+customElements.define("dolly17107-select", Dolly17107Select);
+customElements.define("dolly17107-option", Dolly17107Option);
